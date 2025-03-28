86002084d8631bd78636e738430f0905a00d30b1
riteekagawand <riteekagawand7@gmail.com>
2025-03-28 18:39:31 +0530

    Added multilingual support with Google Translatecls

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,29 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Landing from './home/Landing'
+import Landing from './home/Landing';
 import NonAuthenticatedRoute from './routes/NonAuthenticatedRoute';
-import Login from "./auth/Login"
-import News from "./dashboard/News"
+import Login from './auth/Login';
+import News from './dashboard/News';
+import GoogleTranslate from './components/GoogleTranslator';
 
 function App() {
- 
   return (
     <Router>
-        {/* Define your routes */}
-        <Routes>
-          <Route
-              path="/login"
-              element={
-                // <NonAuthenticatedRoute>
-                  <Login />
-                // </NonAuthenticatedRoute>
-              }
-            />
-            <Route path="/" element={<Landing />} />
-          <Route path='/news' element={<News />} />
-        </Routes>
+      {/* Place GoogleTranslate outside of Routes */}
+      <GoogleTranslate />
+
+      {/* Define your routes */}
+      <Routes>
+        <Route 
+          path="/login" 
+          element={
+            <NonAuthenticatedRoute>
+              <Login />
+            </NonAuthenticatedRoute>
+          } 
+        />
+        <Route path="/" element={<Landing />} />
+        <Route path="/news" element={<News />} />
+      </Routes>
     </Router>
   );
 }
